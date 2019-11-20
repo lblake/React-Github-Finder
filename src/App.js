8218a9ab00035169f5c2bbd5fecf8ce75814dbf9
Lloyd <lloyd@lloyds-mbp.home>
2019-11-20 18:41:17 +0000

    Removed wrong className

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import Navbar from './components/layouts/Navbar';
+import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <nav className='navbar bg-primary'>
-        <Navbar title='Github Finder' icon='fab fa-github'/>
-      </nav>
+      <div className='App'>
+        <Navbar />
+        <div className='container'>
+          <Users />
+        </div>
+      </div>
     );
   }
 }
